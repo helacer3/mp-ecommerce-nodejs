024a4bffa860d80ebc031b3e852a87a9d57a8d6a
Snayder Acero <helacer3@yahoo.es>
2021-04-25 18:52:39 -0500

    ajuste notificacion 5

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ app.get("/pending", function (req, res) {
 
 app.post("/notification", function (req, res, body) {
   console.log("Respuesta Notificacion: ");
-  console.log(body);
-  console.log(res.body);
+  console.log(req.body);
   res.send('POST request to the homepage');
   //console.log("ruta notificacion: ", __dirname + "/notification.txt");
   //fs.writeFile( __dirname + "/notification.txt", data);
